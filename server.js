a567de754d3d5ae7e57b1d46625e9db11bb65b90
unknown <AkshayA@TECH-LP-62.techaspect.com>
2016-08-26 11:55:23 +0530

    changes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ httpApp.post('/sendQuery', function(req, res) {
 
   // setup e-mail data with unicode symbols
   var mailOptions = {
-    to: 'aks.7.arora@gmail.com', // list of receivers
+    to: 'akshay9892@outlook', // list of receivers
     subject: 'Email from ak6arora.github.io', // Subject line
     text: 'Name:'+ JSON.parse(data.toString())['name'] +'\nEmail:'+JSON.parse(data.toString())['email']+'\nMessage:'+JSON.parse(data.toString())['message'], // plaintext body
     html: '<div>Name:'+ JSON.parse(data.toString())['name'] +'<br/>Email:'+JSON.parse(data.toString())['email']+'<br/>Message:'+JSON.parse(data.toString())['message']+'</div>' // html body
